3f7a297d4ae808b91e31d3f8eeafb9161562c320
Md. Shariar Bin Alam <abir3935@gmail.com>
2025-06-23 16:03:54 +0600

    Update Task- 1.java

--- Java Lab/Lab- 6/Task- 1.java
+++ Java Lab/Lab- 6/Task- 1.java
@@ -1,1 +1,37 @@
+class GrandFather {
+    String name = "Putin";
 
+    void company() {
+        System.out.println(name + " Owns a company");
+    }
+}
+
+class Father extends GrandFather {
+    public Father() {
+        this.name = "Trump";
+    }
+
+    void car() {
+        System.out.println(name + " Owns a car");
+    }
+}
+
+class Son  extends Father {
+    public Son(String n ) {
+        this.name = n;
+    }
+
+    void doesNothing() {
+        System.out.println("Eat sleep Drive Car, Repeat");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Son son = new Son("Kim");
+
+        son.company();
+        son.car();
+        son.doesNothing();
+    }
+}
